c8f08778a88cccd03089d08b20d824180c1f246c
Baran Wang <me@baran.wang>
2025-11-26 22:11:38 +0800

    chore: remove favicon link from _renderer.tsx

--- apps/trakt-forward/app/routes/_renderer.tsx
+++ apps/trakt-forward/app/routes/_renderer.tsx
@@ -7,7 +7,6 @@ export default jsxRenderer(({ children }) => {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="icon" href="/favicon.ico" />
         <Link href="/app/style.css" rel="stylesheet" />
         <Script src="/app/client.ts" async />
       </head>
